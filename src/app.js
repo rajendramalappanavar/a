28858877ecfd59fa9fc83e9de22112d6eade752b
iamrajendra <rajendramalappanavar@gmail.com>
2020-01-21 14:34:40 +0530

    sf

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Rajendra'
     })
 });
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Rajendra'
     })
 });
 
@@ -84,7 +84,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Rajendra',
         errorMessage: 'Help article not found.'
     })
 });
@@ -92,7 +92,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Rajendra',
         errorMessage: 'Page not found.'
     })
 });
