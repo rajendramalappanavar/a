d1e9f7bf190308444fc78a0a014e8975a7b6c3bf
rajendramalappanavar <59605361+rajendramalappanavar@users.noreply.github.com>
2020-04-18 22:44:40 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: "hi iam rajendra",
+        helpText: req.body,
         title: 'Help',
         name: 'Rajendra'
     })
